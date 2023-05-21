a7129598291c9203e8d92e84960335352cfbc216
Sacha Duperret <sacha.duperret@u-bordeaux.fr>
2023-05-21 23:28:31 +0200

    initial commit

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,19 +2,19 @@ module.exports = {
 	"title": "TITRE DU SITE",
 	"url": "https://url_du_site.gouv.fr/",
 	"newsletter": {
-		"url": "",
-		"title": "",
-		"description": ""
+		"url": "test",
+		"title": "Test-site",
+		"description": "Test-description"
 	},
-	"facebook_url": "",
-	"twitter_url": "",
-	"instagram_url": "",
-	"linkedin_url": "",
-	"youtube_url": "",
+	"facebook_url": "void",
+	"twitter_url": "void",
+	"instagram_url": "void",
+	"linkedin_url": "void",
+	"youtube_url": "void",
 	"language": "fr",
-	"description": "DESCRIPTION DU SITE",
+	"description": "DESCRIPTION DU SITE_TEST",
 	"author": {
-		"name": "NOM DU SERVICE",
+		"name": "PRESIDENCE",
 		"email": "contact@nom_du_service.gouv.fr",
 		"url": "https://url_du_service.gouv.fr/"
 	}
